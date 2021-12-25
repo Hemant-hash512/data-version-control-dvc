37b9abc0ecd7fc16b0cca5d0b7650f25a03db9e6
Hemant Singh <hemant@exafluence.com>
2021-12-26 02:52:52 +0530

    updated with column

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv(r"C:\Users\DELL\mine\Data_dvc\data\newdata.csv")
+df = pd.read_csv(r"D:\Data-version-control\data\data.csv")
 
 df.drop('has_diabetes', inplace=True, axis=1)
 #print(df)
@@ -9,5 +9,6 @@ diabetes_map = {True:1, False:0}
 df['diabetes'] = df['diabetes'].map(diabetes_map)
 #print(df.head())
 df.drop('thickness', inplace=True, axis=1)
+df.to_csv('data\data1.csv')
 print(df.head())
 
